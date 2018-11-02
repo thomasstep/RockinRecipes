88ce8948fc8771332316c503252399a37600396a
tstep916 <tstep916@tamu.edu>
2018-11-02 11:24:28 -0500

    Added users with likes and dislikes.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import django
 from ioFunctions import loadRecipesJson
 
 fileNames = ["chickenRecipes.json", "beefRecipes.json", "porkRecipes.json", "tofuRecipes.json", "riceRecipes.json", "beansRecipes.json", "americanRecipes.json", "chineseRecipes.json", "mexicanRecipes.json", "italianRecipes.json"]
+recipes = []
 for fileName in fileNames:
-    #print(fileName)
-    loadRecipesJson(fileName)
+    #   print(fileName)
+    recipeGenre = loadRecipesJson(fileName)
+    recipes.append(recipeGenre["recipes"])
+print(recipes)
