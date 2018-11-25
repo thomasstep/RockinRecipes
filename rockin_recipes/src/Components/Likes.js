d7eb0eebd2623eb9212e40422839d03068cd701a
hpadela <hpadela@tamu.edu>
2018-11-25 04:40:34 -0600

    likes class

---
+++ rockin_recipes/src/Components/Likes.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class Likes extends React.Component {
+
+  constructor(props){
+
+    super(props);
+    this.state = {
+      likes: 0,
+      updated: false
+    };
+
+  }
+
+  updateLikes = () => {
+
+    if(!this.state.updated) {
+      this.setState((prevState, props) => {
+        return {
+          likes: prevState.likes + 1,
+          updated: true
+        };
+      });
+
+    } else {
+
+      this.setState((prevState, props) => {
+        return {
+          likes: prevState.likes - 1,
+          updated: false
+        };
+      });
+
+    }
+  }
+
+  render(){
+
+    return(
+      <div>
+        <p onClick={this.updateLikes}>Like</p>
+        <p>{this.state.likes}</p>
+      </div>
+    );
+  }
+}
+
+export default Likes;
