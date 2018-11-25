b70d1535d825e50e4d53a78c7c9abf9b9863e785
Shawn Simon <ShawnSimon@Shawns-MacBook-Pro.local>
2018-11-25 14:49:34 -0600

    Implemented drop down menu with food genres

--- rockin_recipes/src/Components/Recommendations.js
+++ rockin_recipes/src/Components/Recommendations.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from "react-redux";
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
 import { addRecommendationsAction } from "../redux/actions.js"
 
 class Recommendations extends Component {
@@ -8,56 +10,124 @@ class Recommendations extends Component {
         super();
         this.state = {
             recipes: [],
-            recipeId: ""
+            recipeId: "",
+            foodType: "",
+            foodSelection: ''
         }
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+        this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+        this.handleSelectSubmit = this.handleSelectSubmit.bind(this);
     }
 
-    onSubmit(values){
-        this.setState({recipes: []});
-        axios.get(`http://localhost:5000/getRecommendations?recipeId=${values.RecipeID}`)
+    onSubmit(values) {
+        this.setState({ recipes: [] });
+        axios.get(`http://localhost:5000/getRecommendations?recipeId=${values.RecipeID}`)
             .then(res => {
-            this.setState({recommendations: res.data})
-            for ( var i = 0; i < 10; i++){
-                axios.get(`http://localhost:5000/getRecipe?recipeId=${this.state.recommendations[i]}`)
-                    .then(res => {
-                    // Either this gets super huge or recipes is always empty so no need to concat
-                    var newData = this.state.recipes.concat([res.data])
-                    this.setState({recipes: newData});
-                })
-            }
-        })
+                this.setState({ recommendations: res.data })
+                for (var i = 0; i < 10; i++) {
+                    axios.get(`http://localhost:5000/getRecipe?recipeId=${this.state.recommendations[i]}`)
+                        .then(res => {
+                            // Either this gets super huge or recipes is always empty so no need to concat
+                            var newData = this.state.recipes.concat([res.data])
+                            this.setState({ recipes: newData });
+                        })
+                }
+            })
     }
 
     handleChange(event) {
-        this.setState({recipeId: event.target.value});
+        this.setState({ recipeId: event.target.value });
+    }
+
+    handleSearchChange(event) {
+        this.setState({ foodType: event.target.value });
+    }
+
+    handleSelect(event) {
+        this.setState({ foodSelection: event.target.value });
     }
 
     handleSubmit(event) {
         event.preventDefault();
         var recommendationsList = [];
-        axios.get(`http://localhost:5000/getRecommendations?recipeId=${this.state.recipeId}`)
+        axios.get(`http://localhost:5000/getRecommendations?recipeId=${this.state.recipeId}`)
             .then(res => {
                 var recommendations = res.data;
-                for ( var i = 0; i < 10; i++){
+                for (var i = 0; i < 10; i++) {
                     axios.get(`http://localhost:5000/getRecipe?recipeId=${recommendations[i]}`)
                         .then(res => {
                             recommendationsList.push(res.data);
-                    });
+                        });
                 }
                 this.props.addRecommendationsAction(recommendationsList);
             });
     }
 
-    render() {
+    handleSearchSubmit(event) {
+        event.preventDefault();
+        var foodRecommendationsList = [];
+        axios.get(`http://localhost:5000/getSearchResults?query=${this.state.foodType}`)
+            .then(res => {
+                var recommendations = res.data;
+                for (var i = 0; i < 10; i++) {
+                    axios.get(`http://localhost:5000/getRecipe?recipeId=${recommendations[i]}`)
+                        .then(res => {
+                            foodRecommendationsList.push(res.data);
+                        });
+                }
+                this.props.addRecommendationsAction(foodRecommendationsList);
+            });
+    }
+
+    handleSelectSubmit(event) {
+        event.preventDefault();
+        var foodRecommendationsList = [];
+        if(this.state.foodSelection != ""){
+            axios.get(`http://localhost:5000/getSearchResults?query=${this.state.foodSelection}`)
+                .then(res => {
+                    var recommendations = res.data;
+                    for (var i = 0; i < 10; i++) {
+                        axios.get(`http://localhost:5000/getRecipe?recipeId=${recommendations[i]}`)
+                            .then(res => {
+                                foodRecommendationsList.push(res.data);
+                            });
+                    }
+                    this.props.addRecommendationsAction(foodRecommendationsList);
+                });
+        }
+    }
+
+
+    render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     Please enter a recipe ID&nbsp;
-                    <input type="text" value={this.state.recipeId} onChange={this.handleChange}></input><br/>
-                    <input type="submit" value="Get Recommendations"></input><br/>
+                    <input type="text" value={this.state.recipeId} onChange={this.handleChange}></input><br />
+                    <input type="submit" value="Get Recommendations"></input><br />
                 </form>
+                <form onSubmit={this.handleSearchSubmit}>
+                    Please enter a food&nbsp;
+                    <input type="text" value={this.state.foodType} onChange={this.handleSearchChange}></input><br />
+                    <input type="submit" value="Get Food Recomendations"></input><br />
+                </form>
+                <form onSubmit={this.handleSelectSubmit}>
+                    <label>
+                        Pick your Food Genre:
+                        <select value={this.state.foodSelection} onChange={this.handleSelect}>
+                            <option value="" selected> -- Select an Option --</option>
+                            <option value="American">American</option>
+                            <option value="Chinese">Chinese</option>
+                            <option value="Italian">Italian</option>
+                            <option value="Mexican">Mexican</option>
+                        </select>
+                    </label>
+                    <input type="submit" value="Submit"></input><br />
+                </form>
+
             </div>
         )
     }
