71f5745e05a90eb8b48570b45505d514cd97b01b
Shawn Simon <ShawnSimon@Shawns-MacBook-Pro.local>
2018-11-24 22:51:29 -0600

    Query search implemented on frontend

--- rockin_recipes/src/Components/Recommendations.js
+++ rockin_recipes/src/Components/Recommendations.js
@@ -8,10 +8,14 @@ class Recommendations extends Component {
         super();
         this.state = {
             recipes: [],
-            recipeId: ""
+            recipeId: "",
+            foodType: ""
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+        this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+
     }
 
     onSubmit(values){
@@ -34,6 +38,10 @@ class Recommendations extends Component {
         this.setState({recipeId: event.target.value});
     }
 
+    handleSearchChange(event) {
+        this.setState({foodType: event.target.value});
+    }
+
     handleSubmit(event) {
         event.preventDefault();
         var recommendationsList = [];
@@ -50,6 +58,22 @@ class Recommendations extends Component {
             });
     }
 
+    handleSearchSubmit(event) {
+        event.preventDefault();
+        var foodRecommendationsList = [];
+        axios.get(`http://localhost:5000/getSearchResults?query=${this.state.foodType}`)
+            .then(res => {
+                var recommendations = res.data;
+                for ( var i = 0; i < 10; i++){
+                    axios.get(`http://localhost:5000/getRecipe?recipeId=${recommendations[i]}`)
+                        .then(res => {
+                            foodRecommendationsList.push(res.data);
+                    });
+                }
+                this.props.addRecommendationsAction(foodRecommendationsList);
+            });
+    }
+
     render() {
         return (
             <div>
@@ -58,6 +82,12 @@ class Recommendations extends Component {
                     <input type="text" value={this.state.recipeId} onChange={this.handleChange}></input><br/>
                     <input type="submit" value="Get Recommendations"></input><br/>
                 </form>
+                <form onSubmit={this.handleSearchSubmit}>
+                    Please enter a food&nbsp;
+                    <input type="text" value={this.state.foodType} onChange={this.handleSearchChange}></input><br/>
+                    <input type="submit" value="Get Food Recomendations"></input><br/>
+                </form>
+                
             </div>
         )
     }
@@ -83,3 +113,4 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Recommendations)
+
